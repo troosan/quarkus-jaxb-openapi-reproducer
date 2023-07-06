eb0b5c1dc5ae3f29545eb61cf89446bfff3f5202
troosan <antoine.de.troostembergh@gmail.com>
2023-07-06 08:43:05 +0200

    tentative with smallrye-open-api 3.4.1-SNAPSHOT

--- src/main/java/org/acme/JerseyMapperProvider.java
+++ src/main/java/org/acme/JerseyMapperProvider.java
@@ -6,15 +6,16 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 
-import javax.ws.rs.ext.ContextResolver;
-import javax.ws.rs.ext.Provider;
+//import javax.ws.rs.ext.ContextResolver;
+//import javax.ws.rs.ext.Provider;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
+import com.fasterxml.jackson.module.jakarta.xmlbind.JakartaXmlBindAnnotationModule;
+//import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 
-//import jakarta.ws.rs.ext.ContextResolver;
-//import jakarta.ws.rs.ext.Provider;
+import jakarta.ws.rs.ext.ContextResolver;
+import jakarta.ws.rs.ext.Provider;
 
 /**
  * Provides the {@link ObjectMapper} to use to serialize to json
@@ -26,7 +27,8 @@ public class JerseyMapperProvider implements ContextResolver<ObjectMapper> {
 	private static final Logger LOG = Logger.getLogger(JerseyMapperProvider.class);
 
 	static {
-		mapper.registerModule(new JaxbAnnotationModule());
+//		mapper.registerModule(new JaxbAnnotationModule());
+		mapper.registerModule(new JakartaXmlBindAnnotationModule());
 		mapper.findAndRegisterModules();
 		mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, true);
 		mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
--- src/main/java/org/acme/model/HelloResponse.java
+++ src/main/java/org/acme/model/HelloResponse.java
@@ -1,14 +1,14 @@
 package org.acme.model;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
+//import javax.xml.bind.annotation.XmlAccessType;
+//import javax.xml.bind.annotation.XmlAccessorType;
+//import javax.xml.bind.annotation.XmlElement;
+//import javax.xml.bind.annotation.XmlType;
 
-//import jakarta.xml.bind.annotation.XmlAccessType;
-//import jakarta.xml.bind.annotation.XmlAccessorType;
-//import jakarta.xml.bind.annotation.XmlElement;
-//import jakarta.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlType;
 
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlType(name = "Response", propOrder = { "response" })
